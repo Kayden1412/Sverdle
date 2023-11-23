1194d8d0e335adb975b2e1c396a089a2fda1e9b1
Kayden1412 <143221653+Kayden1412@users.noreply.github.com>
2023-11-23 15:04:58 +0700

    Update Header.svelte

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+	import logo from './svelte-logo.svg';
+	import github from './github.svg';
 </script>
 
 <header>
