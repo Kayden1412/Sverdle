7aac471c800375fbc1e9f31ccf8c2aab22ba2f9b
Kayden1412 <143221653+Kayden1412@users.noreply.github.com>
2023-11-25 05:04:54 +0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Wordle clone made with SvelteKit" />
 </svelte:head>
 
 <section>
