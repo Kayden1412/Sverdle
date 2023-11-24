d3b85f1dde25559721eb5bd9e3cf43708121a5ab
Kayden1412 <143221653+Kayden1412@users.noreply.github.com>
2023-11-24 21:49:20 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,9 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-node only supports node environments, such as Replit Autoscale or Reserved VM.
-    // If you'd like to change your Replit Deployment type, see https://kit.svelte.dev/docs/building-your-app
-    // for more information on SvelteKit Adapters
-		adapter: adapter({
-      out: 'build'
-    }),
+	  adapter: adapter(),
 	},
 };
 
